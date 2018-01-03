07c1eb51a26a5dfab6d300448d5f2c900f8584dc
Mark Ma <mazheng1022@hotmail.com>
2018-01-03 16:15:25 +0800

    gun pick up

---
+++ Assets/Scripts/pickUpWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpWeapon : MonoBehaviour {
+
+	public GameObject pickupBtn;
+	public GameObject crosshair;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name == "pistolPickUp")
+		{
+			pickupBtn.gameObject.SetActive (true);
+			crosshair.gameObject.SetActive (false);
+		}
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if (col.gameObject.name == "pistolPickUp")
+		{
+			pickupBtn.gameObject.SetActive (false);
+			crosshair.gameObject.SetActive (true);
+		}
+	}
+}
